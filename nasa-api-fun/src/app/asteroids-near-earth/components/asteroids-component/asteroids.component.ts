23d834a0672e1f0e937c9e4f57f8778061d23ea0
HimatJutla <himatdeveloper@gmail.com>
2018-09-13 19:46:58 -0400

    refactored inputs and outputs to pass service call down to asteroids component through a container handler

--- nasa-api-fun/src/app/asteroids-near-earth/components/asteroids-component/asteroids.component.ts
+++ nasa-api-fun/src/app/asteroids-near-earth/components/asteroids-component/asteroids.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'asteroids-component',
   templateUrl: './asteroids.component.html',
   styleUrls: ['./asteroids.component.css']
 })
-export class AsteroidsComponent {
+export class AsteroidsComponent implements OnInit {
 
   @Input() asteroidsNearEarthInput: any[];
   expandedAsteroidView: boolean = false;
+
+  @Output()
+  viewAsteroids: EventEmitter<any> = new EventEmitter<any>();
+
+  ngOnInit() {}
+
+  toggleAsteroidsView() {
+    this.expandedAsteroidView = !this.expandedAsteroidView;
+
+    if (this.expandedAsteroidView) {
+      this.viewAsteroids.emit();
+    }
+  }
+
 }
--- nasa-api-fun/src/app/asteroids-near-earth/container/asteroids-near-earth-container.component.ts
+++ nasa-api-fun/src/app/asteroids-near-earth/container/asteroids-near-earth-container.component.ts
@@ -11,44 +11,36 @@ export class AsteroidsNearEarthContainerComponent implements OnInit{
   today = new Date();
   todayModified: any;
   todayFinalModification: any;
-  myTestFlag: boolean = false;
   asteroidNearEarthArray: any[];
-  //nearEarthAsteroidsDisplay: any;
 
   constructor(private asteroidsNearEarthService: AsteroidsNearEarthService) {}
   ngOnInit() {
-    this.callNearEarthAsteroidsService();
+      this.callNearEarthAsteroidsService();
   }
 
   callNearEarthAsteroidsService() {
     this.asteroidsNearEarthService
       .getNearEarthAsteroids()
       .subscribe((data: any) => this.nearEarthAsteroids = data);
-      this.todayModified = this.today.getFullYear()+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate();
-      this.getMonthZeroed(this.todayModified);
   }
 
-  logTestData() {
-    // console.log(this.nearEarthAsteroids.near_earth_objects);
-    // console.log(this.nearEarthAsteroids.near_earth_objects[this.todayFinalModification]);
+  handleViewAsteroids(event) {
+    this.todayModified = this.today.getFullYear()+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate();
+    this.getMonthZeroed(this.todayModified);
     this.asteroidNearEarthArray = this.nearEarthAsteroids.near_earth_objects[this.todayFinalModification].map(function (index){
-    return index;
-});
-
-    this.myTestFlag = true;
+      return index;
+    });
   }
 
   getMonthZeroed(date) {
-
     let originalDate = date;
     let monthCheck = date.charAt(5);
     let monthCheckIfOne = date.charAt(6);
     let position = 5;
     let zero = '0';
-    
     if ((monthCheck >= 2 && monthCheck < 10) || (monthCheck == 1 && monthCheckIfOne == "-")) {
       let newModifiedDate = [originalDate.slice(0, position), zero, originalDate.slice(position)].join('');
       this.todayFinalModification = newModifiedDate;
     }
-  }  
+  }
 }
